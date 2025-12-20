80dfbe727e9b3fa8228a62f8e72352c602ac123c
VMBush <slava@MSI>
2025-12-20 19:38:26 +0300

    1

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       name: "mesto-frontend",
       script: "npm",
       args: "start",
-      cwd: "./frontend", // serve из собранной папки
+      // cwd: "./frontend", // serve из собранной папки
       instances: 1,
       autorestart: true,
       watch: false,
