43129fe9a33b902ce42b0f45f721a171d5469c9d
VMBush <slava@MSI>
2025-12-22 00:46:24 +0300

    1

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,33 +1,18 @@
-require("dotenv").config({ path: "./production.env" });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env.deploy" });
 
-const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  KEY_PATH,
-  DEPLOY_REF = "origin/master",
-} = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPOSITORY, DEPLOY_REF } =
+  process.env;
 
 module.exports = {
-  apps: [
-    {
-      name: "mesto-frontend",
-      script: "./dist/app.js",
-    },
-  ],
-
-  // Настройка деплоя
   deploy: {
     production: {
-      key: KEY_PATH,
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: "https://github.com/SilTr4/nodejs-pm2-deploy.git",
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/frontend`,
-      "post-deploy":
-        "cd frontend && npm i && npm run build",
+      "post-deploy": "cd frontend && npm i && npm run build",
     },
   },
 };
